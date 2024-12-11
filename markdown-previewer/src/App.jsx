4bac8d0b424403e07f8543726f806a593288bd27
Thato <albanostlabo0@gmail.com>
2024-12-11 20:28:41 +0200

    solving some error

--- markdown-previewer/src/App.jsx
+++ markdown-previewer/src/App.jsx
@@ -36,49 +36,48 @@ function App() {
   const preview = document.getElementById('preview');
   const editor = document.getElementById('editor')
 
-  bxExpand[0].addEventListener('click', () => {
+
+  const bxExpandZero = () => {
     bxCollapse[0].classList.remove('hide');
     bxExpand[0].classList.add('hide');
     wrapper[1].classList.add('hide');
     wrapper[0].classList.add('width')
     editor.classList.add('height')
-  })
-
-  bxExpand[1].addEventListener('click', () => {
+  }
+  const bxExpandOne = () => {
     bxCollapse[1].classList.remove('hide');
     bxExpand[1].classList.add('hide');
     wrapper[0].classList.add('hide');
     wrapper[1].classList.add('width')
     preview.classList.add('height')
-  })
-
-  bxCollapse[0].addEventListener('click', () => {
+  }
+  const bxCollapseZero = () => {
     bxExpand[0].classList.remove('hide');
     bxCollapse[0].classList.add('hide');
     wrapper[1].classList.remove('hide');
     editor.classList.remove('resize');
     wrapper[0].classList.remove('width')
     editor.classList.remove('height')
-  })
+  }
 
-  bxCollapse[1].addEventListener('click', () => {
+  const bxCollpaseOne = () => {
     bxExpand[1].classList.remove('hide');
     bxCollapse[1].classList.add('hide');
     wrapper[0].classList.remove('hide');
     wrapper[1].classList.remove('width')
     preview.classList.remove('height')
-  })
+  }
 
   return (
     <>
       <div className="editor-wrapper wrapper">Editor
-      <i class='bx bx-expand'></i>
-      <i class='bx bx-collapse-alt hide'></i>
+      <i class='bx bx-expand' onClick={bxExpandZero}></i>
+      <i class='bx bx-collapse-alt hide' onClick={bxCollapseZero}></i>
         <textarea id="editor" onChange={handleChange} value={text}></textarea>
       </div>
       <div className="preview-wrapper wrapper">Preview
-      <i class='bx bx-expand'></i>
-      <i class='bx bx-collapse-alt hide'></i>
+      <i class='bx bx-expand' onClick={bxExpandOne}></i>
+      <i class='bx bx-collapse-alt hide' onClick={bxCollpaseOne}></i>
         <div 
         id="preview"
         dangerouslySetInnerHTML = {{
